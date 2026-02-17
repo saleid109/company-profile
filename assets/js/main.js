79efe4a6df77ea676150871f3855c85bc6371f05
saleid109 <s.aleid.1409@gmail.com>
2026-02-17 19:15:26 +0300

    auto-commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,20 +167,25 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // تفعيل سلايدر الأعمال
-var swiper = new Swiper(".projects-swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+
+const swiper = new Swiper('.projects-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
     },
     breakpoints: {
-        320: { slidesPerView: 1 },
-        768: { slidesPerView: 2 },
-        1024: { slidesPerView: 3 }
+        768: { slidesPerView: 2, spaceBetween: 30 },
+        1024: { slidesPerView: 3, spaceBetween: 40 }
     }
 });
